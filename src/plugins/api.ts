8e17bfa764eebb816fbe582465c5b1c8a25f2fce
Голев Юрий Дмитриевич <yu.d.golev@efko.ru>
2025-03-26 14:34:35 +0300

    Перенес клиент аксиоса в плагин api

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -1,9 +1,16 @@
 import { defineNuxtPlugin } from '#app';
 import todoService from '~/services/todoService';
+import axios from "axios";
 
-export default defineNuxtPlugin((nuxtApp) => {
-    const apiClient = nuxtApp.$apiClient;
+export default defineNuxtPlugin(() => {
+    const config = useRuntimeConfig();
 
+    const apiClient = axios.create({
+        baseURL: config.public.apiBaseUrl,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
     return {
         provide: {
             api: {
